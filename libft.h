0069230adede7049ba9c0c363480daf910bbc431
tokyfy <tokyfydev@gmail.com>
2024-02-23 15:31:15 +0300

    add more five function

--- libft.h
+++ libft.h
@@ -35,12 +35,9 @@ int						ft_tolower(int c);
 
 char					*ft_strchr(const char *s, int c);
 char					*ft_strrchr(const char *s, int c);
-
 int						ft_strncmp(char *s1, char *s2, unsigned int n);
-
 void					*ft_memchr(const void *s, int c, t_size_t n);
 int						ft_memcmp(const void *s1, const void *s2, t_size_t n);
-
 char					*ft_strnstr(const char *str, const char *needle,
 							t_size_t len);
 
@@ -49,4 +46,11 @@ int						ft_atoi(const char *str);
 void					*ft_calloc(t_size_t nmemb, t_size_t size);
 char					*ft_strdup(char *src);
 
+char					*ft_substr(char const *s, unsigned int start,
+							t_size_t len);
+char					*ft_strjoin(char const *s1, char const *s2);
+char					*ft_strtrim(char const *s1, char const *set);
+char					**ft_split(char const *s, char c);
+
+char					*ft_itoa(int n);
 #endif // !LIBFT_H
